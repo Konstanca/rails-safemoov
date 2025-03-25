5befbbd12316aaaa2157c4ced3b66600cadfb83d
Vincent de Cabissole <vince2k@protonmail.com>
2025-03-25 17:16:54 +0100

    address added to registratin + calcul coordinates user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
 
   has_one_attached :photo
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
