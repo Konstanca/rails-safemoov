98e0d8be7c43a9cb741e819c64d1afd05db27030
Vincent de Cabissole <vince2k@protonmail.com>
2025-03-25 16:45:39 +0100

    cloudinary setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    # Autoriser les champs supplémentaires lors de l'inscription
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname, :photo])
+
+    # Autoriser les champs supplémentaires lors de la mise à jour du compte
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :photo])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_24_202327) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_25_151609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "alerts", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -44,8 +72,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_24_202327) do
     t.string "photo_url"
     t.float "latitude"
     t.float "longitude"
-    t.integer "vote_count_plus"
-    t.integer "vote_count_minus"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "date"
@@ -87,9 +113,12 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_24_202327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["incident_id"], name: "index_votes_on_incident_id"
+    t.index ["user_id", "incident_id"], name: "index_votes_on_user_id_and_incident_id", unique: true
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "alerts", "users"
   add_foreign_key "comments", "incidents"
   add_foreign_key "comments", "users"
