b686869921db3d3fd842453391b4fc71160c4e4d
Vincent de Cabissole <vince2k@protonmail.com>
2025-03-31 16:10:31 +0200

    migration read notifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_25_151609) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_31_135502) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,6 +84,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_25_151609) do
     t.bigint "alert_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "read", default: false
     t.index ["alert_id"], name: "index_notifications_on_alert_id"
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
